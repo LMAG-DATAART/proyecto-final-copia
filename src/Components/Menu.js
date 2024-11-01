9ce7350f6055e8fade6ee74760bf1f7e56fa5755
Fer992 <lf.giltr@gmail.com>
2024-11-01 17:14:23 -0500

    Adición de Componente y estilos Notificaciones.Js
    Adición de Componente y estilos Notificaciones.Js  y se modifica Menu, para adicionar funcionamiento de Notificaciones como Alerta

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,75 +2,82 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faClipboardCheck, faHome, faUser, faBriefcase, faBuilding, faCalendarAlt, faBell, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import Notificaciones from './Notificaciones';
 
 const Menu = () => {
     const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+    const [isNotificationsOpen, setNotificationsOpen] = useState(false);
+
+    const toggleNotifications = () => {
+      setNotificationsOpen(!isNotificationsOpen);
+  };
 
     const toggleSubMenu = () => {
         setIsSubMenuOpen(!isSubMenuOpen);
       };
 
-  return (
-    <nav className="menu">
-      <ul>
-        <li>
-          <Link to="/">
-            <FontAwesomeIcon icon={faHome} /> Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/Perfil">
-            <FontAwesomeIcon icon={faUser} /> Perfil
-          </Link>
-        </li>
-        <li>
-          <Link to="/Empleos">
-            <FontAwesomeIcon icon={faBriefcase} /> Empleos
-          </Link>
-        </li>
-        <li>
-          <Link to="/Cursos">
-            <FontAwesomeIcon icon={faBook} /> Cursos
-          </Link>
-        </li>
-        <li>
-          <Link to="/ConsultarAplicaciones">
-            <FontAwesomeIcon icon={faClipboardCheck} /> Consultar Aplicaciones
-          </Link>
-        </li>
-        <li>
-          <Link to="/Empresas">
-            <FontAwesomeIcon icon={faBuilding} /> Empresas
-          </Link>
-        </li>
-        <li onClick={toggleSubMenu}>
-          <span>
-            <FontAwesomeIcon icon={faCalendarAlt} /> Eventos
-          </span>
-            {isSubMenuOpen && (
-                <ul>
-                    <li>
-                        <Link to="/Eventos/HackatonesInternacionales">Hackatones Nacionales</Link>
-                        <Link to="/Eventos/HackatonesNacionales">Hackatones Internacionales</Link>
-                        <Link to="/Eventos/RankingNacional">Ranking Nacional</Link>
-                        <Link to="/Eventos/RankingInternacional">Ranking Internacional</Link>
-                    </li>
-                </ul>
-            )}
-        </li>
-        <li>
-          <Link to="/Notificaciones">
-            <FontAwesomeIcon icon={faBell} /> Notificaciones
-          </Link>
-        </li>
-        <li>
-          <Link to="/Login">
-            <FontAwesomeIcon icon={faSignOutAlt} /> Cerrar Sesión
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+      return (
+        <nav className="menu">
+            <ul>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faHome} /> Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Perfil">
+                        <FontAwesomeIcon icon={faUser } /> Perfil
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Empleos">
+                        <FontAwesomeIcon icon={faBriefcase} /> Empleos
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Cursos">
+                        <FontAwesomeIcon icon={faBook} /> Cursos
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/ConsultarAplicaciones">
+                        <FontAwesomeIcon icon={faClipboardCheck} /> Consultar Aplicaciones
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/Empresas">
+                        <FontAwesomeIcon icon={faBuilding} /> Empresas
+                    </Link>
+                </li>
+                <li onClick={toggleSubMenu}>
+                    <span>
+                        <FontAwesomeIcon icon={faCalendarAlt} /> Eventos
+                    </span>
+                    {isSubMenuOpen && (
+                        <ul>
+                            <li>
+                                <Link to="/Eventos/HackatonesNacionales">Hackatones Nacionales</Link>
+                                <Link to="/Eventos/HackatonesInternacionales">Hackatones Internacionales</Link>
+                                <Link to="/Eventos/RankingNacional">Ranking Nacional</Link>
+                                <Link to="/Eventos/RankingInternacional">Ranking Internacional</Link>
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li onClick={toggleNotifications}>
+                    <span>
+                        <FontAwesomeIcon icon={faBell} /> Notificaciones
+                    </span>
+                </li>
+                <li>
+                    <Link to="/Login">
+                        <FontAwesomeIcon icon={faSignOutAlt} /> Cerrar Sesión
+                    </Link>
+                </li>
+            </ul>
+            <Notificaciones isOpen={isNotificationsOpen} onClose={toggleNotifications} />
+        </nav>
+    );
 };
 
 export default Menu;
--- src/Components/Notificaciones.js
+++ src/Components/Notificaciones.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../Styles/Notificaciones.css' 
 
-const Notificaciones = () => {
+const Notificaciones = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
   return (
-    <div>
-        <h1>Notificaciones</h1>
+    <div className="notificaciones">
+      <div className="notificaciones-header">
+        <h2>Avisos</h2>
+        <button onClick={onClose}>X</button>
+      </div>
+      <div className="notificaciones-body">
+        <h2>Nuevas Ofertas</h2>
+        <p> Revisa las nuevas ofertas para ti</p>
+
+        <h2>Notificaciones</h2>
+        <p> No hay notificaciones pendientes</p>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
 export default Notificaciones;
