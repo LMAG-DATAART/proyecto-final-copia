caaf0384e5ddfad65f84a4fea8f5de12b2e8c47e
Alexa1302 <christinesanchez0903@gmail.com>
2024-11-02 22:26:32 -0500

    Revert "Merge branch 'master' into Rama-Alexa"
    This reverts commit a9c261030072916adcf85737cd5b6415b06611ea, reversing
    changes made to f84a20e82dbbee86a381ff44c207908e389d36e4.

--- src/Components/Notificaciones.js
+++ src/Components/Notificaciones.js
@@ -1,25 +1,11 @@
-import React from 'react';
-import '../Styles/Notificaciones.css' 
+import React from 'react'
 
-const Notificaciones = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
-
+const Notificaciones = () => {
   return (
-    <div className="notificaciones">
-      <div className="notificaciones-header">
-        <h2>Avisos</h2>
-        <button onClick={onClose}>X</button>
-      </div>
-      <div className="notificaciones-body">
-        <h2>Nuevas Ofertas</h2>
-        <p> Revisa las nuevas ofertas para ti</p>
-
-        <h2>Notificaciones</h2>
-        <p> No hay notificaciones pendientes</p>
-      </div>
-      
+    <div>
+        <h1>Notificaciones</h1>
     </div>
-  );
-};
+  )
+}
 
 export default Notificaciones;
