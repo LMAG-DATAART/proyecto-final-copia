b87b04c780c2803776ea9d70cd864e253729233a
Alexa1302 <christinesanchez0903@gmail.com>
2024-11-02 23:25:10 -0500

    Componente Notificaciones
    Re-adición del componente notificaciones y su CSS, se añade el resposive.

--- src/Components/Notificaciones.js
+++ src/Components/Notificaciones.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import styles from "../Styles/Notificaciones.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const Notificaciones = () => {
+
+const Notificaciones = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
   return (
-    <div>
-        <h1>Notificaciones</h1>
+    <div className={styles.notificaciones}>
+      <div className={styles.notificacionesHeader}>
+        <h2>Notificaciones</h2>
+        <button onClick={onClose} className={styles.toggleButton}>
+                <FontAwesomeIcon icon={faTimes} />
+        </button>
+      </div>
+      <div className={styles.notificacionesBody}>
+        <h2>Nuevas Ofertas</h2>
+        <p> Revisa las nuevas ofertas para ti</p>
+        <h2>Notificaciones</h2>
+        <p> No hay notificaciones pendientes</p>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
 export default Notificaciones;
