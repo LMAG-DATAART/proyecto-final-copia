decdd97399ac14384f016062402131a00b1733e0
LMAG-DATAART <luis.alvarez@dataart.com>
2024-11-06 22:19:05 -0500

    Move assets reference folder

--- src/Components/assets.js
+++ src/Components/assets.js
@@ -1,42 +1,42 @@
-import cursoc from "./assets/curso-C++.jpg";
-import cursoJavaScrip from "./assets/Curs-JS.jpg";
-import pythonCurso from "./assets/PYTON-CURSO.jpg";
-import sqlCurso from "./assets/SQL-SERVER-curso.jpg";
-import sqlCursoBasico from "./assets/curso-basico-sql.jpg";
-import cursAngular from "./assets/CursoAngular.jpg";
-import cursonodeJS from "./assets/cursoNodeJS.jpg";
-import cursoGitHub from "./assets/CursoGibHub.jpg";
+import cursoc from "../assets/curso-C++.jpg";
+import cursoJavaScrip from "../assets/Curs-JS.jpg";
+import pythonCurso from "../assets/PYTON-CURSO.jpg";
+import sqlCurso from "../assets/SQL-SERVER-curso.jpg";
+import sqlCursoBasico from "../assets/curso-basico-sql.jpg";
+import cursAngular from "../assets/CursoAngular.jpg";
+import cursonodeJS from "../assets/cursoNodeJS.jpg";
+import cursoGitHub from "../assets/CursoGibHub.jpg";
 
-import microsoftLogo from "./assets/microsoft.png";
-import googleLogo from "./assets/google.png";
-import amazonLogo from "./assets/amazon.png";
-import gitHubLogo from "./assets/gitHub.png";
-import mujerBackend from "./assets/mujer-backend.jpg";
-import mujerMoviles from "./assets/mujerMoviles.jpg";
-import mujerFrontend from "./assets/mujerFrontend.jpg";
-import mujerAsistente from "./assets/mujer-asistente.jpg";
-import mujerNubes from "./assets/MujerNubes.jpeg";
-import mujerGerente from "./assets/mujerGerente.jpg";
-import mujerIngeniera from "./assets/mujerIngeniera.jpg";
+import microsoftLogo from "../assets/microsoft.png";
+import googleLogo from "../assets/google.png";
+import amazonLogo from "../assets/amazon.png";
+import gitHubLogo from "../assets/gitHub.png";
+import mujerBackend from "../assets/mujer-backend.jpg";
+import mujerMoviles from "../assets/mujerMoviles.jpg";
+import mujerFrontend from "../assets/mujerFrontend.jpg";
+import mujerAsistente from "../assets/mujer-asistente.jpg";
+import mujerNubes from "../assets/MujerNubes.jpeg";
+import mujerGerente from "../assets/mujerGerente.jpg";
+import mujerIngeniera from "../assets/mujerIngeniera.jpg";
 
-import img4 from "./assets/img4.png";
-import img5 from "./assets/img5.png";
-import img6 from "./assets/img6.png";
-import img7 from "./assets/img7.png";
+import img4 from "../assets/img4.png";
+import img5 from "../assets/img5.png";
+import img6 from "../assets/img6.png";
+import img7 from "../assets/img7.png";
 
-import img1 from "./assets/img1.png";
+import img1 from "../assets/img1.png";
 
-import logro1 from "./assets/logro1.jpg";
-import logro2 from "./assets/logro2.jpg";
-import curso1 from "./assets/curso1.jpg";
-import curso2 from "./assets/curso2.jpg";
+import logro1 from "../assets/logro1.jpg";
+import logro2 from "../assets/logro2.jpg";
+import curso1 from "../assets/curso1.jpg";
+import curso2 from "../assets/curso2.jpg";
 
-import profileImage from "./assets/fotoPerfilMujer.jpg";
-import profileSugerencia from "./assets/perfilimagen.jpg";
-import empleoImagen from "./assets/empleoNotificacion.jpg";
-import empleoImagen2 from "./assets/EMPLEO2.jpg";
-import empleoImagen3 from "./assets/EMPLEO3.jpg";
-import empleoImagen4 from "./assets/EMPLEO4.jpg";
+import profileImage from "../assets/fotoPerfilMujer.jpg";
+import profileSugerencia from "../assets/perfilimagen.jpg";
+import empleoImagen from "../assets/empleoNotificacion.jpg";
+import empleoImagen2 from "../assets/EMPLEO2.jpg";
+import empleoImagen3 from "../assets/EMPLEO3.jpg";
+import empleoImagen4 from "../assets/EMPLEO4.jpg";
 
 export {
   cursoc,
